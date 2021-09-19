b8c6aef9910d644b62611a19bf239a679cc723f9
Elianagam <elianagam2@gmail.com>
2021-09-19 20:28:28 +0000

    Add seudocode extractToken

--- lab1/src/main/java/ar/uba/fi/compiladores/parte3/ManualLexer.java
+++ lab1/src/main/java/ar/uba/fi/compiladores/parte3/ManualLexer.java
@@ -1,12 +1,16 @@
 package ar.uba.fi.compiladores.parte3;
 
 import java.util.List;
+import java.util.regex.Matcher;
+
+import ar.uba.fi.compiladores.parte1.RegexLibrary;
 
 /**
  * Clase que implementa un algoritmo de lexeo a partir de un lenguage.
  */
 public class ManualLexer<S,T>{
     private LanguageAutomata<S,T> language;
+    RegexLibrary regexLibrary = new RegexLibrary();
     public ManualLexer(LanguageAutomata<S,T> language){
         this.language=language;
     }
@@ -17,6 +21,19 @@ public class ManualLexer<S,T>{
      */
     public SingleTokenExtraction<S> extractToken(String program){
         this.language.eofTokenType();
+
+        S actualState = this.language.getInitialState();
+        S finalState = null;
+        for (int i = 0; i < program.length(); i++) {
+            actualState = this.language.transition(actualState, program.charAt(i));
+            if (this.language.isFinal(actualState)) {
+                finalState = actualState;
+            }
+            if (actualState == this.language.getDeadState()){
+                return new SingleTokenExtraction<S>(finalState, i);    
+            }
+        }
+        
         return null;
     }
     public List<Token<T>> lex(String program){
